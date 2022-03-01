5c3fe9da21a8f1216d30f2e57d3337955e638d89
Myphz <danielscanu45@outlook.it>
2022-03-01 14:03:55 +0100

    Removed unnecessary boilerplate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    Test Home View
+  </div>
 </template>
