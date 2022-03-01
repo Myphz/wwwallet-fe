3955270415052650f9554e88460cb97e65910b28
Myphz <danielscanu45@outlook.it>
2022-03-01 21:04:55 +0100

    Created sass global variables, initialized base view design

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>Test</div>
   <RouterView />
 </template>
 
-<style>
+<style lang="sass">
+  body
+    background-color: $bg-base
+    color: $text-color
+    font-family: $font-base
+    margin: 0 16.66vw
 </style>
