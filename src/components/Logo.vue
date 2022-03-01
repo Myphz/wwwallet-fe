dfe870262b638d525d68808ad571ce4369e472a4
Myphz <danielscanu45@outlook.it>
2022-03-01 21:06:35 +0100

    Added svg logo file, created Icon and Logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="box">
+    <Icon icon="logo" />
+    <span>wwwallet</span>
+  </span>
+</template>
+
+<script setup>
+import Icon from "@/components/Icon.vue"
+</script>
+
+<style lang="sass" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Rambla:ital,wght@1,700&display=swap')
+
+  $logo-font-size: 44px
+  $logo-margin: 10px
+
+  .box
+    display: flex
+    align-items: center
+    font-family: 'Rambla', sans-serif
+    font-size: $logo-font-size
+
+  img
+    margin-right: $logo-margin
+</style>
