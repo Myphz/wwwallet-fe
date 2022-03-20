a63f0bed3ca535168b809ed23c67598690beafa7
Myphz <danielscanu45@outlook.it>
2022-03-20 17:29:45 +0100

    Initialized example chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <ChartOptions :crypto="crypto" :base="base" v-model="base" />
+    <VChart :option="option" />
   </div>
 </template>
 
 <script setup>
-import ChartOptions from "./ChartOptions.vue";
+import ChartOptions from "@/components/ChartOptions.vue";
 import { onMounted, ref } from "vue";
+
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { CandlestickChart } from "echarts/charts";
+import { GridComponent } from "echarts/components";
+import VChart from "vue-echarts";
+
+use([
+  CanvasRenderer,
+  GridComponent,
+  CandlestickChart,
+]);
 
 const borderColor = "#001d3d";
 const textColor = "#003A7B";
@@ -39,6 +52,24 @@ onMounted(() => {
       };
   });
 });
+
+const option = {
+  xAxis: {
+    data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
+  },
+  yAxis: {},
+  series: [
+    {
+      type: 'candlestick',
+      data: [
+        [20, 34, 10, 38],
+        [40, 35, 30, 50],
+        [31, 38, 33, 44],
+        [38, 15, 5, 42]
+      ]
+    }
+  ]
+};
 </script>
 
 <style lang="sass" scoped>
