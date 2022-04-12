a6635bcb82a2f97fd68e2a0160620e874ca8f2eb
Myphz <danielscanu45@outlook.it>
2022-04-12 22:39:50 +0200

    fix: add navbar animation on hover and on current tab (Desktop)

--- src/components/desktop/Navbar.vue
+++ src/components/desktop/Navbar.vue
@@ -2,9 +2,9 @@
   <nav class="noselect">
     <Logo />
     <ul class="h2">
-      <li><RouterLink to="/crypto">Markets</RouterLink></li>
-      <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
-      <li><RouterLink to="/wallet">Wallet</RouterLink></li>
+      <li :class="path == '/crypto' ? 'current-tab' : ''"><RouterLink to="/crypto">Markets</RouterLink></li>
+      <li :class="path == '/dashboard' ? 'current-tab' : ''"><RouterLink to="/dashboard">Dashboard</RouterLink></li>
+      <li :class="path == '/wallet' ? 'current-tab' : ''"><RouterLink to="/wallet">Wallet</RouterLink></li>
     </ul>
     <RouterLink to="/login" class="align-center"><UserIcon class="user-icon" /></RouterLink>
   </nav>
@@ -12,10 +12,12 @@
 
 <script setup>
 import Logo from "U#/Logo.vue"
-import { RouterLink } from "vue-router";
-import { defineAsyncComponent } from "vue";
+import { RouterLink, useRoute } from "vue-router";
+import { computed, defineAsyncComponent } from "vue";
 
 const UserIcon = defineAsyncComponent(() => import("../../assets/icons/user.svg"));
+const route = useRoute();
+const path = computed( () => route.path );
 </script>
 
 <style lang="sass" scoped>
@@ -32,6 +34,12 @@ const UserIcon = defineAsyncComponent(() => import("../../assets/icons/user.svg"
   li
     display: inline-flex
     margin: 0 1em
+    transition: 0.25s all
+    &:hover
+      box-shadow: inset 0 -1px 0 0 $text-primary
+
+  .current-tab
+    box-shadow: inset 0 -1px 0 0 $text-primary
 
   .user-icon
     transform: scale(1.5)
