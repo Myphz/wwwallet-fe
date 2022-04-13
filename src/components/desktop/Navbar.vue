d312f31bd2fff9b46ecbcbb7634f61c885d14774
Myphz <danielscanu45@outlook.it>
2022-04-13 20:13:33 +0200

    fix: navbar animation on hover

--- src/components/desktop/Navbar.vue
+++ src/components/desktop/Navbar.vue
@@ -2,9 +2,9 @@
   <nav class="noselect">
     <Logo />
     <ul class="h2">
-      <li :class="path == '/crypto' ? 'current-tab' : ''"><RouterLink to="/crypto">Markets</RouterLink></li>
-      <li :class="path == '/dashboard' ? 'current-tab' : ''"><RouterLink to="/dashboard">Dashboard</RouterLink></li>
-      <li :class="path == '/wallet' ? 'current-tab' : ''"><RouterLink to="/wallet">Wallet</RouterLink></li>
+      <li><RouterLink to="/crypto">Markets</RouterLink></li>
+      <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
+      <li><RouterLink to="/wallet">Wallet</RouterLink></li>
     </ul>
     <RouterLink to="/login" class="align-center"><UserIcon class="user-icon" /></RouterLink>
   </nav>
@@ -12,12 +12,9 @@
 
 <script setup>
 import Logo from "U#/Logo.vue"
-import { RouterLink, useRoute } from "vue-router";
-import { computed, defineAsyncComponent } from "vue";
-
+import { RouterLink } from "vue-router";
+import { defineAsyncComponent } from "vue";
 const UserIcon = defineAsyncComponent(() => import("../../assets/icons/user.svg"));
-const route = useRoute();
-const path = computed( () => route.path );
 </script>
 
 <style lang="sass" scoped>
@@ -30,18 +27,18 @@ const path = computed( () => route.path );
     list-style-type: none
     margin: 0
     padding: 0
+
+  a
+    transition: 0.25s all
   
   li
     display: inline-flex
     margin: 0 1em
     transition: 0.25s all
     &:hover
-      box-shadow: inset 0 -1px 0 0 $text-primary
-
-  .current-tab
-    box-shadow: inset 0 -1px 0 0 $text-primary
+      a
+        color: darken($text-primary, 10%)
 
   .user-icon
     transform: scale(1.5)
-
 </style>
