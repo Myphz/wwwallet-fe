0cb4b1f21a84045621ab6c940ad719e4eabfcd5b
Myphz <danielscanu45@outlook.it>
2022-04-16 18:47:18 +0200

    fix: add pagination on market view (Mobile)

--- src/components/mobile/markets/Cards.mobile.vue
+++ src/components/mobile/markets/Cards.mobile.vue
@@ -11,10 +11,17 @@
       <Card v-for="i in 10" :key="i" />
     </tbody>
   </table>
+  <section class="justify-center" style="gap: 5em; margin-top: 1em">
+    <ArrowIcon :class="'arrow-icon noselect reverse ' + (page === 1 ? 'inactive' : 'bg-base transition pointer')" @click="page !== 1 ? page-- : null"/>
+    <ArrowIcon class="arrow-icon noselect bg-base transition pointer" @click="page++" />
+  </section>
 </template>
 
 <script setup>
-import Card from "M#/markets/Card.mobile.vue"
+import Card from "M#/markets/Card.mobile.vue";
+import { defineAsyncComponent, ref } from "vue";
+const ArrowIcon = defineAsyncComponent(() => import("../../../assets/icons/arrow.svg"));
+const page = ref(1);
 </script>
 
 <style lang="sass" scoped>
@@ -26,5 +33,19 @@ import Card from "M#/markets/Card.mobile.vue"
     padding: 1em 1.2em
     text-align: left
     font-weight: bold
+</style>
 
+<style lang="sass">
+  .arrow-icon
+    width: 48px
+    height: 48px  
+    path
+      fill: none
+
+  .reverse
+    transform: scaleX(-1)
+
+  .inactive
+    path
+      stroke: $text-secondary  
 </style>
