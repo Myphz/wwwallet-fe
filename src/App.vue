c5d3d1f2da20a1a364caf9ab26e8ce313972082f
Myphz <danielscanu45@outlook.it>
2022-04-16 14:31:13 +0200

    refactor: add vite aliases to store folder

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <script setup>
 import { defineAsyncComponent } from "vue";
 import { routeComponent } from "@/helpers/route.helper";
-import { useCryptoStore } from "./stores/crypto.store";
+import { useCryptoStore } from "S#/crypto.store";
 const Layout = defineAsyncComponent(() => routeComponent("Layout"));
 const store = useCryptoStore();
 store.init();
