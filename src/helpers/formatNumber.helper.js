5fade91c729a09f5b128b011601b74d550937d90
Myphz <danielscanu45@outlook.it>
2022-04-19 11:02:03 +0200

    fix: improve number formatter

--- src/helpers/formatNumber.helper.js
+++ src/helpers/formatNumber.helper.js
@@ -1,6 +1,26 @@
+function getDecimalDigits(n) {
+  const str = n.toString();
+  const index = str.indexOf(".");
+  return index === -1 ? 0 : str.length - index - 1;
+}
+
+function formatBigValue(price) {
+  // Try with billions first
+  let N = price / 1000000000;
+  let Nsuffix = "B";
+  if (N < 1) {
+    N = price / 1000000;
+    Nsuffix = "M";
+  };
+
+  return N.toFixed(3) + Nsuffix;
+};
+
 export const formatValue = price => {
   if (!price) return "0.00";
-  return price.toFixed(2);
+  if (price >= 500000) return formatBigValue(price);
+  const decimalDigits = getDecimalDigits(price);
+  return price.toLocaleString(undefined, { minimumFractionDigits: decimalDigits, maximumFractionDigits: decimalDigits });
 }
 
 export const formatPercentage = pct => {
