98612f1a4e9b74ce417051f4af8bff01e4bfe272
Myphz <danielscanu45@outlook.it>
2022-05-06 21:04:20 +0200

    feature: `UserSettings` view logic (Mobile)

--- src/views/mobile/UserSettings.mobileView.vue
+++ src/views/mobile/UserSettings.mobileView.vue
@@ -21,7 +21,7 @@
   <section class="h4">
     <h1>Security</h1>
 
-    <div class="bg-base transition pointer" @click="emailDialog = true">
+    <div class="bg-base transition pointer" @click="dialogMode = 'email'">
       <div class="align-center">
         <Icon icon="email" class="margin-right" />
         <span>Email</span>
@@ -29,7 +29,7 @@
       <Icon icon="arrow" class="margin-left" />
     </div>
 
-    <div class="bg-base transition pointer" @click="passwordDialog = true">
+    <div class="bg-base transition pointer" @click="dialogMode = 'password'">
       <div class="align-center">
         <Icon icon="password" class="margin-right" />
         <span>Password</span>
@@ -42,7 +42,7 @@
   <section class="h4">
     <h1>Advanced</h1>
 
-    <div class="bg-base transition pointer">
+    <div class="bg-base transition pointer" @click="dialogMode = 'transactions'">
       <div class="align-center">
         <CrossIcon class="margin-right red-stroke" style="transform: scale(.666)" />
         <span class="red">Delete all transactions</span>
@@ -50,7 +50,7 @@
       <Icon icon="arrow" />
     </div>
 
-    <div class="bg-base transition pointer">
+    <div class="bg-base transition pointer" @click="dialogMode = 'account'">
       <div class="align-center">
         <CrossIcon class="margin-right red-stroke" style="transform: scale(.666)" />
         <span class="red">Delete account</span>
@@ -59,10 +59,10 @@
     </div>
   </section>
 
-  <Dialog v-if="emailDialog">
-    <div class="space-between align-center" style="margin-bottom: 1em">
-      <h2 style="white-space: nowrap; margin-right: 1em">Change email</h2>
-      <Icon icon="cross" clickable @click="emailDialog = false" />
+  <Dialog v-if="dialogMode === 'email'">
+    <div class="dialog-header">
+      <h2>Change email</h2>
+      <Icon icon="cross" clickable @click="dialogMode = false" />
     </div>
     <span class="h4">Type your new email to update your account</span>
     <form @submit.prevent="changeEmail" style="margin-top: 3em">
@@ -79,13 +79,31 @@
     </form>
   </Dialog>
 
-  <Dialog v-if="passwordDialog">
-    <div class="space-between align-center" style="margin-bottom: 1em">
-      <h2 style="white-space: nowrap; margin-right: 1em">Change password</h2>
-      <Icon icon="cross" clickable @click="passwordDialog = false" />
+  <Dialog v-else-if="dialogMode === 'password'">
+    <div class="dialog-header">
+      <h2>Change password</h2>
+      <Icon icon="cross" clickable @click="dialogMode = false" />
     </div>
     <span class="h4">Click the button to receive an email to change your current password</span><br>
     <Button btnCss="margin-top: 1em" @click="changePassword">Submit</Button>
+  </Dialog>
+
+  <Dialog v-else-if="dialogMode === 'transactions'">
+    <div class="dialog-header">
+      <h2>Delete transactions</h2>
+      <Icon icon="cross" clickable @click="dialogMode = false" />
+    </div>
+    <span class="h4">Click the button below to continue.<br><strong>All</strong> your transactions will be deleted <i>permanently</i>.</span><br>
+    <Button btnCss="margin-top: 1em" @click="deleteTransactions">Delete all transactions</Button>
+  </Dialog>
+
+  <Dialog v-else-if="dialogMode === 'account'">
+    <div class="dialog-header">
+      <h2>Delete account</h2>
+      <Icon icon="cross" clickable @click="dialogMode = false" />
+    </div>
+    <span class="h4">Click the button below to receive an email to continue.<br>Your account and your data will be <i>permanently deleted</i></span><br>
+    <Button btnCss="margin-top: 1em" @click="deleteAccount">Submit</Button>
   </Dialog>
 
   <Popup :success="popup.success" :message="popup.msg" @endAnimation="popup.success = null" mobile />
@@ -103,8 +121,9 @@ import { defineAsyncComponent, ref } from "vue";
 import { QUOTES } from "@/config/config"
 
 import { validateEmail } from "@/helpers/validator.helper";
-import { fetchServer } from "@/helpers/fetch.helper";
+import { useAuthStore } from "S#/auth.store";
 
+const store = useAuthStore();
 const CrossIcon = defineAsyncComponent(() => import("../../assets/icons/cross.svg"));
 
 const startValue = localStorage.getItem("currency");
@@ -115,27 +134,40 @@ const updateCurrency = newCurrency => {
 };
 
 // User update/delete
-const emailDialog = ref(false);
-const passwordDialog = ref(false);
+const dialogMode = ref(false);
 
 const values = ref({});
 const popup = ref({ success: null, msg: "" });
 
 const changeEmail = async () => {
   if (!values.value.emailValid) return;
-  popup.value = await fetchServer("account/update", { email: values.value.email });
-  emailDialog.value = false;
+  popup.value = await store.changeEmailRequest(values.value.email);
+  dialogMode.value = false;
 };
 
 const changePassword = async () => {
-  popup.value = await fetchServer("account/update", null, { method: "POST" });
-  passwordDialog.value = false;
+  popup.value = await store.changePasswordRequest();
+  dialogMode.value = false;
+};
+
+const deleteTransactions = async () => {
+  popup.value = await store.deleteTransactions();
+  dialogMode.value = false;
+};
+
+const deleteAccount = async () => {
+  popup.value = await store.deleteAccountRequest();
+  dialogMode.value = false;
 };
 </script>
 
 <style lang="sass" scoped>
   h1
     margin-bottom: .5em
+
+  h2
+    white-space: nowrap
+    margin-right: 1em
 
   section
     margin-bottom: 2em
@@ -158,6 +190,12 @@ const changePassword = async () => {
 
   .margin-right
     margin-right: .5em
+
+  .dialog-header
+    display: flex
+    justify-content: center
+    align-items: center
+    margin-bottom: 1em
 </style>
 
 <style lang="sass">
