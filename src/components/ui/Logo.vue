50d566f05379f2cd69fef0ded461f70a2572f0ee
Myphz <danielscanu45@outlook.it>
2022-05-14 10:31:22 +0200

    fix: remove href from Logo

--- src/components/ui/Logo.vue
+++ src/components/ui/Logo.vue
@@ -1,15 +1,12 @@
 <template>
-  <RouterLink custom v-slot="{ navigate }" to="/">
-    <span class="box align-center noselect" @click="navigate">
-      <Icon icon="logo" :class="small ? 'small' : ''" />
-      <h1>wwwallet</h1>
-    </span>
-  </RouterLink>
+  <span class="box align-center noselect">
+    <Icon icon="logo" :class="small ? 'small' : ''" />
+    <h1>wwwallet</h1>
+  </span>
 </template>
 
 <script setup>
 import Icon from "U#/Icon.vue";
-import { RouterLink } from "vue-router";
 
 defineProps({
   small: {
@@ -22,7 +19,6 @@ defineProps({
 <style lang="sass" scoped>
   .box
     font-family: $font-logo
-    cursor: pointer
 
   img
     margin-right: 10px
