23d6e435e666047f322affa776311a9f87813b35
Myphz <danielscanu45@outlook.it>
2022-05-19 23:13:26 +0200

    feature: add sign and release apk build

--- android.sh
+++ android.sh
@@ -1,5 +1,6 @@
 node node_modules/vite/bin/vite.js build --mode android
-rm -rf ./android
+rm -rf ./apk
+mkdir apk
 npx cap add android
 npx cap copy android
 rm -rf ../dist
@@ -19,4 +20,10 @@ public class MainActivity extends BridgeActivity {
   }
 }" > ./app/src/main/java/com/wwwallet/app/MainActivity.java
 
-ANDROID_SDK_ROOT=/opt/android-sdk ./gradlew assembleDebug
+ANDROID_SDK_ROOT=/opt/android-sdk ./gradlew assembleRelease
+# Sign the apk
+cd app/build/outputs/apk/release
+/opt/android-sdk/build-tools/30.0.2/zipalign -v -p 4 app-release-unsigned.apk aligned.apk
+/opt/android-sdk/build-tools/30.0.2/apksigner sign --ks ~/keys/1.0.jks --out ../../../../../../apk/wwwallet.apk aligned.apk
+cd ../../../../../../
+rm -rf ./android
